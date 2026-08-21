571842d6524b4cad2f5122b473fe47529824d583
andresgoncalves11 <andres.goncalves11@gmail.com>
2026-08-21 00:20:45 +0000

    cambio 1

--- prueba.py
+++ prueba.py
@@ -1,3 +1,17 @@
 import os
+os.system ('clear')
 
-print("Hola, ¿como estás?") 
+print("Bienvenido a la prueba") 
+print("Han pasado varios meses")
+print("¿Quieres continuar? S/N")
+respuesta = input("respuesta: ")
+respuesta = respuesta.upper()
+if respuesta == "N":
+    print("Has decidido no continuar.")
+else:
+    print("Mira esta lista")
+    lista = ["A", "B", "C", "D", "E", "F", "G"]
+    print (lista)
+    print ("Mira ahora")
+    lista.append ("Hola")
+    print (lista)
